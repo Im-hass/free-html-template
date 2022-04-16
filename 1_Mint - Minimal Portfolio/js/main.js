b2b48f5234026b448e8129489de783c39b794797
Imhuiseon <huiseon4107@gmail.com>
2022-04-17 02:32:39 +0900

    Feat: nav 스크롤 시 숨기기/보이기 애니메이션 추가

--- 1_Mint - Minimal Portfolio/js/main.js
+++ 1_Mint - Minimal Portfolio/js/main.js
@@ -1,5 +1,6 @@
 const filter_listEl = document.querySelector('.filter_box > ul').children;
 const toTopEl = document.getElementById('to-top');
+const nav = document.getElementsByTagName('nav')[0];
 let aEls = [];
 
 for (let i = 0; i < filter_listEl.length; i++) {
@@ -24,4 +25,25 @@ aEls.forEach(function (el) {
 
 toTopEl.addEventListener('click', function () {
   window.scrollTo(0, 0);
+});
+
+let beforePosition = document.documentElement.scrollTop;
+
+document.addEventListener('scroll', function() {
+    let afterPosition = document.documentElement.scrollTop;
+
+    if (afterPosition > 50) {
+      if(beforePosition < afterPosition ){
+        nav.classList.add('nav-up');
+      } else {
+        nav.classList.remove('nav-up');
+      }
+    } else {
+      if(afterPosition < 120) {
+        nav.classList.remove('nav-up');
+      } else {
+        nav.classList.add('nav-up');
+      }
+    }
+    beforePosition = afterPosition;
 });
